4692d58231b0927c9cd60a0ef78dc4c91578a043
Justin Louie <jslouie@gmail.com>
2016-09-27 21:51:43 +0800

    A little stream lining

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,12 @@ class HomeController < ApplicationController
   def index
     @events = []
 
-    DoorEvent.order(created_at: :desc).limit(100).each_cons(2) do |event, next_event|
+    raw_events = DoorEvent.order(created_at: :desc).limit(100)
+
+    raw_events.each_cons(2) do |event, next_event|
       @events << event if event.action != next_event&.action
     end
 
-    @last_event = DoorEvent.last
+    @latest_event = raw_events.first
   end
 end
