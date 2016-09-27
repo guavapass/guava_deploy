bb49a5442c28b8da54cdaecee749809d4d42e302
Justin Louie <jslouie@gmail.com>
2016-09-27 21:00:03 +0800

    MOAR DATA

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     prev_event = nil
 
-    @events = DoorEvent.order(created_at: :desc).limit(20).select do |event|
+    @events = DoorEvent.order(created_at: :desc).limit(100).select do |event|
       if prev_event&.action != event.action
         prev_event = event
         true
