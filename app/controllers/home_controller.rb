c26754267f6e528f91cebe043c343d25528848e9
Justin Louie <jslouie@gmail.com>
2016-10-02 11:37:29 +0800

    Basing everything off last used

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,17 +4,32 @@ class HomeController < ApplicationController
 
     @todays_visits = Visit.where(start_at: time_now.beginning_of_day .. time_now).order(start_at: :desc)
 
-    yesterday = time_now - 1.day
-    @yesterday_visits = Visit.where(start_at: yesterday.beginning_of_day .. yesterday.end_of_day).order(start_at: :asc)
+    @last_used_at = Visit.last.start_at
+
+    @last_used_day_visits = Visit.where(
+      start_at: @last_used_at.beginning_of_day .. @last_used_at.end_of_day
+    ).order(start_at: :asc)
 
     @icon_color = CurrentState.color
 
-    @yesterday_visits_by_hour = visits_by_hour(@yesterday_visits)
-
-    @total_time = yesterday_total_time(@yesterday_visits)
+    set_stats(@last_used_day_visits)
   end
 
   private
+
+  def set_stats(visits)
+    @last_used_day_visits_by_hour = visits_by_hour(visits)
+
+    @last_used_day_total_time = visits_total_time(visits)
+  end
+
+  def visits_duration_sorted(visits)
+    visits.sort_by do |visit|
+      next unless visit.end_at
+
+      (visit.end_at - visit.start_at).to_i
+    end
+  end
 
   def visits_by_hour(visits)
     visits.group_by do |visit|
@@ -22,11 +37,11 @@ class HomeController < ApplicationController
     end
   end
 
-  def yesterday_total_time(visits)
+  def visits_total_time(visits)
     visits.map do |visit|
       next unless visit.end_at
 
-      visit.end_at.to_i - visit.start_at.to_i
+      (visit.end_at - visit.start_at).to_i
     end.compact.inject(&:+)
   end
 end
