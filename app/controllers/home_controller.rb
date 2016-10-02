812253f3db3020dea27a0c010621d7d09cb901a3
Justin Louie <jslouie@gmail.com>
2016-10-02 13:05:25 +0800

    Adding a few more stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,20 @@
 class HomeController < ApplicationController
   def index
     time_now = Time.zone.now
+    yesterday = time_now - 1.day
 
     @todays_visits = Visit.where(start_at: time_now.beginning_of_day .. time_now).order(start_at: :desc)
 
-    @last_used_at = Visit.last.start_at
+    # Last used that was not today
+    @last_used_at = Visit.where('start_at < ?', yesterday.end_of_day).last.start_at
 
-    @last_used_day_visits = Visit.where(
+    last_used_day_visits = Visit.where(
       start_at: @last_used_at.beginning_of_day .. @last_used_at.end_of_day
     ).order(start_at: :asc)
 
     @icon_color = CurrentState.color
 
-    set_stats(@last_used_day_visits)
+    set_stats(last_used_day_visits)
   end
 
   private
@@ -21,6 +23,8 @@ class HomeController < ApplicationController
     @last_used_day_visits_by_hour = visits_by_hour(visits)
 
     @last_used_day_total_time = visits_total_time(visits)
+
+    @last_used_day_visits = visits_duration_sorted(visits)
   end
 
   def visits_duration_sorted(visits)
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def duration_hms(obj, format = '%H:%M:%S')
+    seconds = if obj.is_a?(Visit)
+                (obj.end_at - obj.start_at).to_i
+              else
+                obj
+              end
+
+    Time.at(seconds).utc.strftime(format)
+  end
 end
